6d5feac632424728bc3516799766ac6b80fdbe75
Aleksandra <a.vasilyeva@onpoint.ru>
2021-03-15 18:19:49 +0300

    add gh pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <p>Onpoint Wiki</p>
+        <p>Testing the deployment</p>
       </header>
     </div>
   )
